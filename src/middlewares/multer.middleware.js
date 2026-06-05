94d2bdb7de26d52f61e1748b4acf16b8b27c3ed2
highjunesolution <highjunesolution@gmail.com>
2026-06-05 10:29:39 +0700

    fix: limit single upload file

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -107,7 +107,7 @@ function singleUploadMiddleware(type) {
   return (req, res, next) => {
     const upload = multer({
       storage,
-      limits: { fileSize: DEFAULT_MAX_SIZE, files: DEFAULT_MAX_FILES },
+      limits: { fileSize: DEFAULT_MAX_SIZE, files: 1 },
       fileFilter: makeFileFilter(type),
     }).single("file");
 
